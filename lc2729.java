c99e3c8be05ff3936ef9bf85b8c21d329db35526
KOSIREDDY VASANTH KUMAR <digitalvasanth01@gmail.com>
2024-03-06 10:20:52 +0530

    lc2729.java
    2729. Check if The Number is Fascinating

---
+++ lc2729.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isFascinating(int n) {
+        
+        int n1=2*n;
+        int n2=3*n;
+        
+        //adding given number (n1 and n2) in string format
+        String concat=n+""+n1+n2;
+        
+        
+        for(char c='1';c<='9';c++){
+            int count=0;
+        for(int i=0;i<concat.length();i++){
+            char ch=concat.charAt(i);
+            
+            if(ch==c)
+            count++;
+        }
+   //if character appeared more than once or not appeared at all the trend broken
+            if(count>1||count==0)
+                return false;
+        }
+        return true;
+    }
+}
